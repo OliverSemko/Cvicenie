ac0b57d8517c915fea3316898433626ed97d6b54
OliverSemko <79322735+OliverSemko@users.noreply.github.com>
2021-02-20 12:58:19 +0100

    Update Cvicenie1.java

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie1.java
@@ -3,6 +3,12 @@ package sk.stuba.fei.uim.oop;
 public class Cvicenie1 {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        ding
+            ding
+                ding
+                    ding
+                        ding
+                            DONG
 
     }
 }
